182be4a0161ba3b2e13b0d95dcc9892dad2de7b3
sfali23 <f.syed.ali@gmail.com>
2021-01-21 19:23:18 -0500

    refactor ButtonBuilder

--- core/src/main/scala/com/alphasystem/primereact/component/button/ButtonBuilder.scala
+++ core/src/main/scala/com/alphasystem/primereact/component/button/ButtonBuilder.scala
@@ -7,50 +7,93 @@ import io.kinoplan.scalajs.react.bridge.WithProps
 
 import scala.scalajs.js.JSConverters._
 
-case class ButtonBuilder(
-  label: Option[String] = None,
-  icon: Option[Either[String, Icon]] = None,
-  iconPos: Option[IconPos] = None,
-  severity: Option[Severity] = None,
-  size: Option[ButtonSize] = None,
-  link: Boolean = false,
-  raised: Boolean = false,
-  rounded: Boolean = false,
-  outlined: Boolean = false,
-  text: Boolean = false,
-  badge: Option[String] = None,
-  badgeType: Option[BadgeType] = None) {
+class ButtonBuilder {
+  private var label: Option[String] = None
+  private var icon: Option[String] = None
+  private var iconPos: Option[IconPos] = None
+  private var classNames: List[String] = Nil
+  private var badge: Option[String] = None
+  private var badgeType: Option[BadgeType] = None
 
-  private def toClassName: Option[String] = {
-    var classNames: List[String] = Nil
-    severity.map(_.toClassName).foreach(classNames :+= _)
-    size.map(_.toClassName).foreach(classNames :+= _)
-    if (link) classNames :+= "p-button-link"
-    if (raised) classNames :+= "p-button-raised"
-    if (rounded) classNames :+= "p-button-rounded"
-    if (outlined) classNames :+= "p-button-outlined"
-    if (text) classNames :+= "p-button-text"
-    if (classNames.isEmpty) None else Some(classNames.mkString(" "))
+  def label(label: String): ButtonBuilder = {
+    this.label = Some(label)
+    this
   }
 
-  private def toIconClass: Option[String] =
-    icon match {
-      case Some(value) =>
-        value match {
-          case Left(value)  => Some(value)
-          case Right(value) => Some(value.toClassName)
-        }
-      case None => None
-    }
+  def icon(icon: Icon): ButtonBuilder = {
+    this.icon = Some(icon.toClassName)
+    this
+  }
 
-  def toButton: WithProps = {
+  def icon(icon: String): ButtonBuilder = {
+    this.icon = Some(icon)
+    this
+  }
+
+  def iconPos(iconPos: IconPos): ButtonBuilder = {
+    this.iconPos = Some(iconPos)
+    this
+  }
+
+  def badge(badge: String): ButtonBuilder = {
+    this.badge = Some(badge)
+    this
+  }
+
+  def badgeType(badgeType: BadgeType): ButtonBuilder = {
+    this.badgeType = Some(badgeType)
+    this
+  }
+
+  def severity(severity: Severity): ButtonBuilder = {
+    this.classNames :+= severity.toClassName
+    this
+  }
+
+  def size(size: ButtonSize): ButtonBuilder = {
+    this.classNames :+= size.toClassName
+    this
+  }
+
+  def link: ButtonBuilder = {
+    this.classNames :+= "p-button-link"
+    this
+  }
+
+  def raised: ButtonBuilder = {
+    this.classNames :+= "p-button-raised"
+    this
+  }
+
+  def rounded: ButtonBuilder = {
+    this.classNames :+= "p-button-rounded"
+    this
+  }
+
+  def outlined: ButtonBuilder = {
+    this.classNames :+= "p-button-outlined"
+    this
+  }
+
+  def text: ButtonBuilder = {
+    this.classNames :+= "p-button-text"
+    this
+  }
+
+  private def toClassName: Option[String] =
+    if (classNames.isEmpty) None else Some(classNames.mkString(" "))
+
+  def toButton: WithProps =
     Button(
       label = label.orUndefined,
       className = toClassName.orUndefined,
-      icon = toIconClass.orUndefined,
+      icon = icon.orUndefined,
       iconPos = iconPos.orUndefined,
       badge = badge.orUndefined,
       badgeClassName = badgeType.map(_.toClassName).orUndefined
     )
-  }
+}
+
+object ButtonBuilder {
+  def apply(): ButtonBuilder = new ButtonBuilder()
 }
